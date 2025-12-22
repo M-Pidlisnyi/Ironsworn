1a808bd15759010cd22b2f616ccbd882deefc839
M-Pidlisnyi <mishapropatriot@gmail.com>
2025-12-22 13:35:15 +0200

    django defaul auth basic

--- ironsworn/urls.py
+++ ironsworn/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gameplay.urls')),
     path('play/', include('gameplay.urls')),
+    path('characters/', include('characters.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
