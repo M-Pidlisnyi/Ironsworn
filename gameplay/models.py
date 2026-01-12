d1efacee9dd7542e444d6378da90ff503a5d35c4
misha <misha@test.com>
2026-01-12 21:45:03 +0200

    gameplay/story models

--- gameplay/models.py
+++ gameplay/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Story(models.Model):
+    world = models.ForeignKey("worlds.World", on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    prologue = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    participants = models.ManyToManyField("characters.Character", through="StoryParticipant")
+
+class Event(models.Model):
+    story = models.ForeignKey(Story, on_delete=models.CASCADE, related_name="events")
+    character = models.ForeignKey("characters.Character", 
+                                  on_delete=models.SET_NULL, null=True, blank=True,
+                                  help_text="Character primarily responsible for this event, if any.")
+    move = models.ForeignKey("rules.Move", on_delete=models.SET_NULL, null=True, blank=True,
+                                    help_text="Move that was made during the event, if any.")
+    
+
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    order = models.PositiveIntegerField()
+    
+
+class StoryParticipant(models.Model):
+    story = models.ForeignKey(Story, on_delete=models.CASCADE)
+    participant = models.ForeignKey("characters.Character", on_delete=models.CASCADE, related_name='stories')
+
+    class Meta:
+        unique_together = ("story", "participant")
